dcd9a9cb483cbfe122d375e7b3dce3f436a10c8c
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2018-09-13 19:54:40 +0430

    working with style

--- App.js
+++ App.js
@@ -1,53 +1,69 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+    Dimensions,
+    Image,
+    ImageBackground,
+    StyleSheet,
+    Text,
+    View,
+    StatusBar
+} from 'react-native';
+import picSierra from './assets/Sierra-Spencer.png'
+import picTanner from './assets/Tanner-McTab.png'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <Text style={styles.red}>Finaly Setup</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={style.container}>
+                <StatusBar hidden={true}/>
+                <ImageBackground style={style.pic} source={picSierra}>
+                    <Text style={style.userName}>Sierra</Text>
+                </ImageBackground>
+                <ImageBackground style={style.pic} source={picTanner}>
+                    <Text style={style.userName}>Tanner</Text>
+                </ImageBackground>
+                {/*<Text style={style.defaultText}>Sierra</Text>
+                <Text style={[style.defaultText,style.selectedText]}>Tanner</Text>
+                <Text style={style.defaultText}>Travis</Text>*/}
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  red: {
-    color: '#f00',
-  },
+const style = StyleSheet.create({
+    container:{
+        backgroundColor:'#DDD',
+        flex:1,
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    defaultText: {
+        flex:1,
+        textAlign:'center',
+        fontSize: 22,
+        padding: 10,
+        margin:5,
+        borderWidth:StyleSheet.hairlineWidth,
+        color:'#000'
+    },
+    selectedText: {
+        flex:2,
+        backgroundColor:'yellow',
+        color:'blue',
+        fontWeight:'bold'
+    },
+    pic:{
+        flex:1,
+        width:Dimensions.get('window').width,
+    },
+    userName: {
+        backgroundColor: 'rgba(0,0,0,.8)',
+        fontSize: 30,
+        padding: 10,
+        color: 'white',
+        position:'absolute',
+        bottom: 0,
+        right:0
+    }
 });
