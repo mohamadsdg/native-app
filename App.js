140d3f36ee872fc2aeb69b274b95e5efc70483dd
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2018-09-18 10:50:13 +0430

    response to touch with onPress

--- App.js
+++ App.js
@@ -8,23 +8,43 @@ import {
     View,
     StatusBar
 } from 'react-native';
+
+//-- import img
 import picSierra from './assets/Sierra-Spencer.png'
 import picTanner from './assets/Tanner-McTab.png'
+//-- End import img
 
-export default class App extends Component<Props> {
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            backgroundColor: "blue"
+        };
+
+        this.onChangeColor = this.onChangeColor.bind(this)
+
+    }
+
+    onChangeColor(backgroundColor){
+
+        this.setState({backgroundColor});
+    }
+
     render() {
+        const {backgroundColor} = this.state;
         return (
-            <View style={style.container}>
-                <StatusBar hidden={true}/>
-                <ImageBackground style={style.pic} source={picSierra}>
-                    <Text style={style.userName}>Sierra</Text>
-                </ImageBackground>
-                <ImageBackground style={style.pic} source={picTanner}>
-                    <Text style={style.userName}>Tanner</Text>
-                </ImageBackground>
-                {/*<Text style={style.defaultText}>Sierra</Text>
-                <Text style={[style.defaultText,style.selectedText]}>Tanner</Text>
-                <Text style={style.defaultText}>Travis</Text>*/}
+            <View style={
+                [
+                    {backgroundColor:backgroundColor},
+                    style.container
+                ]}>
+                <StatusBar hidden={false}/>
+                <Text
+                    style={style.button}
+                    onPress={() => this.onChangeColor("green")}>green</Text>
+                <Text
+                    style={style.button}
+                    onPress={() => this.onChangeColor("red")}>red</Text>
             </View>
         );
     }
@@ -32,38 +52,19 @@ export default class App extends Component<Props> {
 
 const style = StyleSheet.create({
     container:{
-        backgroundColor:'#DDD',
         flex:1,
-        flexDirection:'column',
-        justifyContent:'center',
-        alignItems:'center',
+        justifyContent: "center",
+        alignItems : "center",
     },
-    defaultText: {
-        flex:1,
+    button:{
+        fontSize:25,
+        padding:15,
+        margin:15,
+        borderWidth:2,
+        borderColor:"#000",
+        fontWeight:"bold",
+        alignSelf:'stretch',
         textAlign:'center',
-        fontSize: 22,
-        padding: 10,
-        margin:5,
-        borderWidth:StyleSheet.hairlineWidth,
-        color:'#000'
-    },
-    selectedText: {
-        flex:2,
-        backgroundColor:'yellow',
-        color:'blue',
-        fontWeight:'bold'
-    },
-    pic:{
-        flex:1,
-        width:Dimensions.get('window').width,
-    },
-    userName: {
-        backgroundColor: 'rgba(0,0,0,.8)',
-        fontSize: 30,
-        padding: 10,
-        color: '#fff',
-        position:'absolute',
-        bottom: 0,
-        right:0
+        borderRadius:10
     }
 });
