38bf4093fb810b6c0ee5617de993a28868a377bc
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2018-11-26 10:52:27 +0330

    native-base

--- src/component/sideBar.js
+++ src/component/sideBar.js
@@ -4,7 +4,10 @@ import {
     Header,
     Container,
     Text,
-    View
+    View,
+    List,ListItem,
+    Left,Right,Body,
+    Button,Icon
 } from 'native-base'
 
 import {
@@ -16,11 +19,232 @@ class SideBar extends Component {
     render() {
         return (
             <Container style={styles.SideBar_style}>
+                <Image source={require('../../assets/pic/head-150.jpg')}/>
                 <Content>
-                    <Image source={require('../../assets/pic/head-150.jpg')}/>
-                    <View>
-                        <Text>Hello</Text>
-                    </View>
+                    <List>
+                        <ListItem >
+                            <Left>
+                                <Text>Native Base</Text>
+                            </Left>
+                            <Body/>
+                            <Right>
+                                <Button primary rounded style={{width:87,height:30}}>
+                                    <Text style={{fontSize:12}}>Primary</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem icon>
+                            <Left>
+                                <Icon name='alert' style={{color:'green',fontSize:23}}/>
+                            </Left>
+                            <Body>
+                                <Text>Badge</Text>
+                            </Body>
+                            <Right>
+                                <Button primary style={{width:75,height:25}}>
+                                    <Text style={{fontSize:12}}>8 Type</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem icon>
+                            <Left>
+                                <Icon name='train' style={{color:'red',fontSize:23}}/>
+                            </Left>
+                            <Body>
+                            <Text>Button</Text>
+                            </Body>
+                            <Right>
+                                <Button primary style={{width:75,height:25}}>
+                                    <Text style={{fontSize:12}}>8 Type</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem >
+                            <Left>
+                                <Text>Native Base</Text>
+                            </Left>
+                            <Body/>
+                            <Right>
+                                <Button primary rounded style={{width:87,height:30}}>
+                                    <Text style={{fontSize:12}}>Primary</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem icon>
+                            <Left>
+                                <Icon name='alert' style={{color:'green',fontSize:23}}/>
+                            </Left>
+                            <Body>
+                            <Text>Badge</Text>
+                            </Body>
+                            <Right>
+                                <Button primary style={{width:75,height:25}}>
+                                    <Text style={{fontSize:12}}>8 Type</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem icon>
+                            <Left>
+                                <Icon name='train' style={{color:'red',fontSize:23}}/>
+                            </Left>
+                            <Body>
+                            <Text>Button</Text>
+                            </Body>
+                            <Right>
+                                <Button primary style={{width:75,height:25}}>
+                                    <Text style={{fontSize:12}}>8 Type</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem >
+                            <Left>
+                                <Text>Native Base</Text>
+                            </Left>
+                            <Body/>
+                            <Right>
+                                <Button primary rounded style={{width:87,height:30}}>
+                                    <Text style={{fontSize:12}}>Primary</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem icon>
+                            <Left>
+                                <Icon name='alert' style={{color:'green',fontSize:23}}/>
+                            </Left>
+                            <Body>
+                            <Text>Badge</Text>
+                            </Body>
+                            <Right>
+                                <Button primary style={{width:75,height:25}}>
+                                    <Text style={{fontSize:12}}>8 Type</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem icon>
+                            <Left>
+                                <Icon name='train' style={{color:'red',fontSize:23}}/>
+                            </Left>
+                            <Body>
+                            <Text>Button</Text>
+                            </Body>
+                            <Right>
+                                <Button primary style={{width:75,height:25}}>
+                                    <Text style={{fontSize:12}}>8 Type</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem >
+                            <Left>
+                                <Text>Native Base</Text>
+                            </Left>
+                            <Body/>
+                            <Right>
+                                <Button primary rounded style={{width:87,height:30}}>
+                                    <Text style={{fontSize:12}}>Primary</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem icon>
+                            <Left>
+                                <Icon name='alert' style={{color:'green',fontSize:23}}/>
+                            </Left>
+                            <Body>
+                            <Text>Badge</Text>
+                            </Body>
+                            <Right>
+                                <Button primary style={{width:75,height:25}}>
+                                    <Text style={{fontSize:12}}>8 Type</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem icon>
+                            <Left>
+                                <Icon name='train' style={{color:'red',fontSize:23}}/>
+                            </Left>
+                            <Body>
+                            <Text>Button</Text>
+                            </Body>
+                            <Right>
+                                <Button primary style={{width:75,height:25}}>
+                                    <Text style={{fontSize:12}}>8 Type</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem >
+                            <Left>
+                                <Text>Native Base</Text>
+                            </Left>
+                            <Body/>
+                            <Right>
+                                <Button primary rounded style={{width:87,height:30}}>
+                                    <Text style={{fontSize:12}}>Primary</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem icon>
+                            <Left>
+                                <Icon name='alert' style={{color:'green',fontSize:23}}/>
+                            </Left>
+                            <Body>
+                            <Text>Badge</Text>
+                            </Body>
+                            <Right>
+                                <Button primary style={{width:75,height:25}}>
+                                    <Text style={{fontSize:12}}>8 Type</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem icon>
+                            <Left>
+                                <Icon name='train' style={{color:'red',fontSize:23}}/>
+                            </Left>
+                            <Body>
+                            <Text>Button</Text>
+                            </Body>
+                            <Right>
+                                <Button primary style={{width:75,height:25}}>
+                                    <Text style={{fontSize:12}}>8 Type</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem >
+                            <Left>
+                                <Text>Native Base</Text>
+                            </Left>
+                            <Body/>
+                            <Right>
+                                <Button primary rounded style={{width:87,height:30}}>
+                                    <Text style={{fontSize:12}}>Primary</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem icon>
+                            <Left>
+                                <Icon name='alert' style={{color:'green',fontSize:23}}/>
+                            </Left>
+                            <Body>
+                            <Text>Badge</Text>
+                            </Body>
+                            <Right>
+                                <Button primary style={{width:75,height:25}}>
+                                    <Text style={{fontSize:12}}>8 Type</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                        <ListItem icon>
+                            <Left>
+                                <Icon name='train' style={{color:'red',fontSize:23}}/>
+                            </Left>
+                            <Body>
+                            <Text>Button</Text>
+                            </Body>
+                            <Right>
+                                <Button primary style={{width:75,height:25}}>
+                                    <Text style={{fontSize:12}}>8 Type</Text>
+                                </Button>
+                            </Right>
+                        </ListItem>
+                    </List>
                 </Content>
             </Container>
         );
